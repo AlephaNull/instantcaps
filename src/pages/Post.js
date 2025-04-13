c816210021b17ba4dc495d04b390c542d7ba06af
Ayaan Syed <ayaan.s4512@gmail.com>
2025-04-13 16:13:40 +0530

    Addded dialog box to upload links

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,34 +1,47 @@
 import Link from "next/link"
 import Image from "next/image"
+import { useState } from "react";
 import { Heart, MessageCircle, Bookmark, Send, MoreHorizontal } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 
+import { Input } from "@/components/ui/input"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyACXuqPtmQvYJjDpx8eeohthOBff2CUfSA");
-
-const model = genAI.getGenerativeModel({ model: 'models/gemini-1.5-pro' });
-
-const imageResp = await fetch(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg/2560px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg'
-)
-    .then((response) => response.arrayBuffer());
-
-const result = await model.generateContent([
-    {
-        inlineData: {
-            data: Buffer.from(imageResp).toString("base64"),
-            mimeType: "image/jpeg",
-        },
-    },
-    'write a small instagram caption for this image. make it under 50 words and make it sound genz. remove all formatting. give me only one',
-]);
-console.log(result.response.text());
+// const genAI = new GoogleGenerativeAI("AIzaSyACXuqPtmQvYJjDpx8eeohthOBff2CUfSA");
+//
+// const model = genAI.getGenerativeModel({ model: 'models/gemini-1.5-pro' });
+//
+// const imageResp = await fetch(
+//     'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg/2560px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg'
+// )
+//     .then((response) => response.arrayBuffer());
+//
+// const result = await model.generateContent([
+//     {
+//         inlineData: {
+//             data: Buffer.from(imageResp).toString("base64"),
+//             mimeType: "image/jpeg",
+//         },
+//     },
+//     'write a small instagram caption for this image. make it under 50 words and make it sound genz. remove all formatting. give me only one',
+// ]);
+// console.log(result.response.text());
 export default function InstagramPost() {
+  const [link, setLink] = useState()
   return (
     <div className="flex justify-center w-full p-4">
       <Card className="max-w-md w-full overflow-hidden">
@@ -43,6 +56,20 @@ export default function InstagramPost() {
             <span className="sr-only">More options</span>
           </Button>
         </CardHeader>
+            <Dialog>
+              <DialogTrigger>Open</DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Link to Image</DialogTitle>
+                  <DialogDescription className="flex w-full max-w-sm items-center space-x-2">
+                    {/* <p className="flex w-full max-w-sm items-center space-x-2"> */}
+                      <Input type="email" placeholder="Link" value={link} onChange={e => setLink(e.target.value)} />
+                      <Button type="submit" onClick={e => console.log(link)}>Use</Button>
+                    {/* </p> */}
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
         <CardContent className="p-0">
           <div className="relative aspect-square">
             <Image
